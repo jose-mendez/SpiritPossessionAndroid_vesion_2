3b6003aa8bf31769511b954e32901a344d6ba8ea
Jose Mendez <pepe_jmendez23@hotmail.com>
2013-12-30 18:05:14 -0500

    1st commit

---
+++ SpiritPossession - Free/src/com/spiritpossession/TextToSpeechClass.java
@@ -0,0 +1,32 @@
+package com.spiritpossession;
+
+import java.util.Locale;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.content.Intent;
+import java.util.Locale;
+
+import com.spiritpossession.R;
+import com.spiritpossession.R.layout;
+
+import android.widget.Toast;
+
+public class TextToSpeechClass extends Activity  {
+   
+	
+	
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+                //get a reference to the button element listed in the XML layout
+           
+         
+    }
+       
+    
+        //speak the user text
+    
+}
